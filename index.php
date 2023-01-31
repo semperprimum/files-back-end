db311a6dc7db6b650d4ea3ebd584b7404689f684
semperprimum <bkeem0266+github@gmail.com>
2023-01-31 19:29:51 +0600

    second commit

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     date_default_timezone_set('Asia/Almaty');
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     function putData () {
       $data = file_get_contents("comment.json");
       $arr_data = json_decode($data);
@@ -10,10 +9,12 @@
         'date_and_time' => date('j F H:i')
       ];
       $arr_data[] = $array;
-      return json_encode($arr_data);
+      $encoded = json_encode($arr_data);
+      file_put_contents("comment.json", $encoded);
     }
-    file_put_contents("comment.json", putData());
-  }
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      putData();
+    }
 ?>
 
 <!DOCTYPE html>
@@ -31,17 +32,38 @@
             margin-top: 10px;
         }
     </style>
-<form method="post">
+<form method="POST" action="">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Ваше имя</label>
-    <input type="text" class="form-control" placeholder="Имя" id="exampleInputEmail1 name" name="name" aria-describedby="emailHelp">
+    <input type="text" class="form-control" placeholder="Имя" id="exampleInputEmail1" name="name" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Текст сообщения</label>
-    <input type="text" class="form-control" placeholder="Текст" id="exampleInputPassword1 comment" name="comment">
+    <input type="text" class="form-control" placeholder="Текст" id="exampleInputPassword1" name="comment">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+<table class="table">
+<tr>
+  <th>Name</th>
+  <th>Comment</th>
+  <th>Time</th>
+</tr>
+  <?php 
+if (file_exists('comment.json')) {
+  $data = file_get_contents("comment.json");
+  $arr_data = json_decode($data, true);
+  foreach ($arr_data as $comment) {
+    echo "<tr>";
+    echo "<td>". $comment['name'] . '</td>';
+    echo "<td>". $comment['comment'] . '</td>';
+    echo "<td>". $comment['date_and_time'] . '</td>';
+    echo "</tr>";
+  }
+}
+?>
+
+</table>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
